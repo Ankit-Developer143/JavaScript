0a13ffd17552b72d711c6dc339ab7f305aad7e23
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-05-30 21:39:31 +0530

    Java Script

---
+++ JS/String.js
@@ -0,0 +1,17 @@
+mystring="i am programmer";
+console.log(mystring)
+
+//Augmented 
+myStr="this is pain."+"this is lollypop";
+console.log(myStr)
+
+myvalue="hello"
+myname= " oye" +myvalue+ "my name is ankit";
+console.log(myname)
+
+
+//Appending variable in String
+myName="Ankit";
+myStr="My Name is";
+myStr+=myName;
+console.log(myStr)
