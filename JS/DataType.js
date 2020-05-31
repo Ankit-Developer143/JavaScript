5187f38c1197557194b551b05833a1fd52654389
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-05-31 22:23:55 +0530

    All JavaScript properties

---
+++ JS/DataType.js
@@ -0,0 +1,169 @@
+
+/*Primitive data type (Store in Stack)
+String:-"Ankit"
+number:-34
+Boolean:-true,false(small letter)
+null
+symbol:-s6 latest
+
+//reference datatype(object) Store in Heap 
+Array:-[]
+object literals
+function 
+Dates
+always show its datatype object exc
+*/
+
+//String Datatype
+let Name="ram";
+console.log(Name)
+console.log("its datatype is:"+(typeof Name));
+
+//Number datatype
+let marks=34;
+console.log("the value of marks is:"+marks+"\t\nthe datatype is:"+(typeof marks));
+
+
+//Boolean
+let drive=true;
+console.log("the datatype is:"+(typeof drive))
+
+//null
+let nullvar=null;
+console.log("the data type is:"+(nullvar))
+
+//undefined
+let undefinedvar=undefined;
+console.log("the data type is:"+(undefinedvar))
+
+//reference object datatype
+
+//Arrays
+let myarr=[1,2,3,4,"string",false]
+console.log("the datatype is:"+(typeof myarr))
+
+//object literal
+setmarks= {
+    ram:34,
+    shyam:22,
+    Ankit:45
+    
+}
+console.log(setmarks);
+console.log("the datatype is:"+(typeof setmarks))
+
+function findname() {
+    
+}
+console.log(typeof findname);//op: its function
+
+let date=new Date();
+console.log(typeof date);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let name="Ankit";
+console.log(name)
