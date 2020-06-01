9ce73fcb0a6ba08926d74914e01ce2f76144c5ae
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-06-01 21:25:08 +0530

    loops

---
+++ JS/For_While_Do_While_Loops.js
@@ -0,0 +1,32 @@
+
+//for loop
+for(let i=0;i<10;i++){
+    console.log(i);
+}
+
+let arr1 = [2,4,5,6,4,5,4];
+
+for(let i=0;i<arr.length;i++){
+    const element= arr1[i];
+    console.log(element);
+}
+
+//do while loop atleast one time run
+let result='';
+let j=0;
+do{
+    j=j+1;
+    result=result+j;
+    
+}while(j<5);
+console.log(result);
+
+//While loop
+
+let n=0;
+while (n<3) {
+    n++;
+    
+}
+console.log(n);
+
