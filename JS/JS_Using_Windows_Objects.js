d3eebc7cae18dbea470fd8c980187110e1c3a49b
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-06-05 18:25:42 +0530

    Window Object

---
+++ JS/JS_Using_Windows_Objects.js
@@ -0,0 +1,25 @@
+var a=window.document;
+a= alert("This is not Secure page");
+
+a=prompt("this page can not accesiable","Enter your name below");
+
+if yes then its give boolean values
+a=confirm("Are you sure to continue to this page")
+
+a=window.innerHeight; //op:-538 depend on window size
+
+a=window.innerWidth;//write window are optional
+
+a=window.scrollX;//op:-depend on scrooling page
+a=window.scrollY;
+
+a=location;//op:-Location {href: "http://127.0.0.1:57871/Main.html",
+          // ancestorOrigins: DOMStringList, origin: "http://127.0.0.1:57871", protocol: "http:", 
+          //host: "127.0.0.1:57871", …}
+
+a=location.toString();//op:-JS_Using_Windows_Objects.js:23 http://127.0.0.1:57871/Main.html
+
+a=window.history;//op:-History {length: 3, scrollRestoration: "auto", state: null}
+
+
+console.log(a);
