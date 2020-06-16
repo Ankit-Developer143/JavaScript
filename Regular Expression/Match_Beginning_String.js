f5652a747beab5403645e2cf66d7644b1b01b911
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-06-16 18:52:10 +0530

    find beginning element

---
+++ Regular Expression/Match_Beginning_String.js
@@ -0,0 +1,8 @@
+var character = "Hi my name is ankit singh";
+var match = /[^Hi]/;
+var result = character.match(match);
+console.log(result);
+//op:-Hi
+//op:-[hi^]if we use this then  the op is :-i
+//if we use this [hi^] then the op is :- H
+
