787f7e44ccab572e99b13a67c09e7b10043daa66
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-06-19 21:06:29 +0530

    combine to array

---
+++ Array Prototype/spread.js
@@ -0,0 +1,7 @@
+//its use to combine to operator
+    let fragment = ['to', 'code'];
+    let sentence = ['learning',...fragment,'is','fun'] // Change this line
+    
+  console.log(sentence);
+
+  //op:-[ 'learning', 'to', 'code', 'is', 'fun' ]
