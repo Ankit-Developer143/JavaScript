13784a48aad1390c3c573b3e3521a2d1af8c7ce3
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-06-19 20:47:58 +0530

    Prototype

--- Array Prototype/Array.slice().js
+++ Array Prototype/Array.slice().js
@@ -17,3 +17,14 @@ console.log(animals.slice(2, 4));//(Start,end)
 
 console.log(animals.slice(1, 5));
 // expected output: Array ["bison", "camel", "duck", "elephant"]
+
+function forecast(arr) {
+    // Only change code below this line
+  arr.slice(2,4);
+    return arr.slice(2,4);;
+  }
+  
+  // Only change code above this line
+  console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+  //op:-[ 'warm', 'sunny' ]
+  
