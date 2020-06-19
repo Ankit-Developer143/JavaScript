9970db863080b8d6c21b1e2ac522c0d2a4c263e1
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-06-19 21:29:32 +0530

    Prototype

---
+++ Array Prototype/Array.indexOf().js
@@ -0,0 +1,11 @@
+const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+
+console.log(beasts.indexOf('bison'));
+// expected output: 1
+
+// start from index 2
+console.log(beasts.indexOf('bison', 2));
+// expected output: 4
+
+console.log(beasts.indexOf('Ankit'));//if data not present then its give negative values
+// expected output: -1
