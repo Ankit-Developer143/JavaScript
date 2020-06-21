5d1a4acc84a55402a0c072a85aa284c46d980764
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-06-21 20:25:49 +0530

    Jump Statment

---
+++ Jump Statment/Label_Statment.js
@@ -0,0 +1,8 @@
+//label Statment specify name to specify the block that can refer it to elsewhere in the program
+//identifer:statment
+let a=1;
+
+label:while(a==1){
+    console.log(a);
+    break;
+}
---
+++ Jump Statment/break_Statment.js
@@ -0,0 +1,10 @@
+//break statment 
+for(let i=0;i<=5;i++){  //
+   if(i==3)
+   console.log(i);
+       break label;
+  
+   
+}
+
+
