137303549c8630e9b5b2d78681b82ba34f1b6d43
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-06-22 22:38:21 +0530

    Set map

---
+++ Map/Set_map.js
@@ -0,0 +1,8 @@
+/* let myMap = new Map()
+myMap.set('bla','blaa')
+myMap.set('bla2','blaa2')
+console.log(myMap)  */
+
+let myVariable = new Map()
+myVariable.set('Name','Ankit')
+console.log(myVariable);
