80d456ac56fcabf64af73fbf74e896d8f4d6d180
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-06-22 22:37:29 +0530

    for each

---
+++ Map/ForEach.js
@@ -0,0 +1,9 @@
+const Value = new Map();
+Value.set('Name', 'Ankit');
+Value.set('Address', 'Laxmi nagar')
+
+
+//for Each
+Value.forEach(function (a, b) {
+    console.log(a + "=" + b);  /* choose any value(Variable) and also select number of parameter */
+});
