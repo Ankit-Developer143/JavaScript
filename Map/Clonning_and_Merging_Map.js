587b9c3eb5dc5b640d18a7161f691d707b47b27f
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-06-22 22:43:34 +0530

    Map

---
+++ Map/Clonning_and_Merging_Map.js
@@ -0,0 +1,14 @@
+let first = new Map([
+    [1, 'one'],
+    [2, 'two'],
+    [3, 'three'],
+  ])
+  let second = new Map([
+    [1, 'uno'],
+    [2, 'dos']
+  ])
+  let merged = new Map([...first,...second]);
+  console.log(merged);
+  /*  Merge maps with an array. The last repeated key wins.
+  Map { 1 => 'uno', 2 => 'dos', 3 => 'three' }
+  */
