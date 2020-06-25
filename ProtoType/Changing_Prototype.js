9625fa1d3898fc4c05204a140d75dc93170e9e58
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-06-25 18:43:34 +0530

    Prototype

---
+++ ProtoType/Changing_Prototype.js
@@ -0,0 +1,9 @@
+function Student() {
+    this.name = "Ankit"
+    this.gender = "Male"
+}
+Student.prototype.age = 15;
+var obj1 = new Student();
+console.log(obj1.age);//op:-15
+
+    
---
+++ ProtoType/Object_Prototype.js
@@ -0,0 +1,16 @@
+function Student() {
+    this.name = 'John';
+    this.gender = 'M';
+}
+
+var studObj = new Student();
+
+Student.prototype.sayHi = function(){
+    console.log("hello");
+
+};
+var studObj1 = new Student();
+var proto = Object.getPrototypeOf(studObj1);  // returns Student's prototype object
+            
+console.log(proto.constructor);
+//op:-[Function: Student]
