3344c6b072444f65656581150e282281d68505dd
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-06-28 20:28:53 +0530

    Ternary Operator

---
+++ Ternary Operator/Using.js
@@ -0,0 +1,13 @@
+//condition ? condition_if_true : condition_if_false
+
+function sum(a) {
+   let result = ( a%2==0? "even":"odd"); 
+   console.log(result);
+}
+var obj = new sum(4)//even
+
+    
+//Simple Example 
+var age = 26;
+var beverage = (age >= 21) ? "Beer" : "Juice";
+console.log(beverage); // "Beer"
