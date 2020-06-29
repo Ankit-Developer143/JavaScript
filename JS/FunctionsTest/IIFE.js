1bdbaeaef0934cff79d3a3f46e43a6d73ed8f37f
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-06-29 19:59:07 +0530

    immediately Invoked Function Expression

---
+++ JS/FunctionsTest/IIFE.js
@@ -0,0 +1,11 @@
+/*  Understand the Immediately Invoked Function Expression (IIFE)
+A common pattern in JavaScript is to execute a function as soon as it is declared: */
+
+(function() {
+    console.log("A cozy nest is ready");
+  })();
+
+  //op:-A cozy nest is ready
+
+  /* Condition:-The function should be anonymous.
+  Your function should have parentheses at the end of the expression to call it immediately.() */
