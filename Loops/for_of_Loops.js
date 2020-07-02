6a0c6f0f6ad2103051c47c9ff637fe764870dc96
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-02 21:16:39 +0530

    for of loops

---
+++ Loops/for_of_Loops.js
@@ -0,0 +1,13 @@
+
+
+var animals = ['Gertrude', 'Henry', 'Melvin', 'Billy Bob']
+
+for(let animal of animals){
+    
+    console.log(animal.toUpperCase());
+}
+/* op:-
+ GERTRUDE
+HENRY
+MELVIN
+BILLY BOB */
