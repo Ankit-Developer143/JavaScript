123bb729fa11d08287b066e7c31092eb54a039dc
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-02 21:19:03 +0530

    While

---
+++ Loops/While_Loops_All_Types.js
@@ -0,0 +1,6 @@
+let i = 0;
+while (i < 100) {
+    i++;               /* if we here use i++ then loop start with 1 to 10 */
+    console.log(i);
+    i++;             /* if we here use i++ then loop start with 0 to 9 */
+}
