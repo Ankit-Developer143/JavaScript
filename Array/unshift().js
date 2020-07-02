e5b79b2b52fd7feffa5a2368b77d82e33c57f64c
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-02 21:39:40 +0530

    unshift

---
+++ Array/unshift().js
@@ -0,0 +1,9 @@
+//Add new items to the beginning of an array:
+var fruits = ['Banana', 'Mango', 'Orange' ];
+var result = fruits.unshift("Apple");
+console.log(result);
+console.log(fruits);
+
+/* both op 
+4
+[ 'Apple', 'Banana', 'Mango', 'Orange' ] */
