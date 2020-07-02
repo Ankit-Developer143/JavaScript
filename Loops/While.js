50200b776d4dbba3bbca5d03bc1349876f1cf3ef
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-02 21:17:21 +0530

    While'

---
+++ Loops/While.js
@@ -0,0 +1,13 @@
+let x=0;
+let n = 0;
+while (n<3) {
+    n++;
+   // x=n;   if we use this condition and print then the op both are same like x=1,n=1
+    x+=n;
+    console.log("he value of n is "  +n+ "  the Value of X is  " +x);
+}
+/* op:- 
+he value of n is 1  the Value of X is  1
+he value of n is 2  the Value of X is  3
+he value of n is 3  the Value of X is  6
+*/
