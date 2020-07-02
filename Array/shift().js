90e62a2eb3589173e731e766e929c216c97447a3
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-02 21:35:54 +0530

    Shift

---
+++ Array/shift().js
@@ -0,0 +1,9 @@
+//Remove first  (index[0] )element on array
+
+var fruits = [ 'Apple', 'Banana', 'Mango', 'Orange' ];
+var result = fruits.shift();
+console.log(result);
+console.log(fruits);
+
+/* op
+[ 'Banana', 'Mango', 'Orange' ] */
