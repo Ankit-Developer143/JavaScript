8e65a360107226d363fab12a3a0b7f11e62c1249
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-02 21:15:14 +0530

    do while loops

---
+++ Loops/do_While_Loops.js
@@ -0,0 +1,7 @@
+let i = 0;
+do {
+    //i++;                  /* if we here use i++ then loop start with 1 to 10 */
+    console.log(i);
+    i++;                /* if we here use i++ then loop start with 0 to 9 */
+    
+} while (i <10);
