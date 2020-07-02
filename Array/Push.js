0f2b00a6461a20a1a24089489dced2b2ed7ea98d
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-02 18:06:22 +0530

    Push()

---
+++ Array/Push.js
@@ -0,0 +1,8 @@
+/* ADD ELEMENT IN THE LAST IN ARRAY */
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var obj = fruits.push("Kiwi");
+console.log(obj);
+console.log(fruits);
+
+/* op:- [ 'Banana', 'Orange', 'Apple', 'Mango', 'Kiwi' ] */
