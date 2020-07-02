b4f173022dd49fc505ad43848ae96664c08b4771
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-02 17:55:42 +0530

    pop()

---
+++ Array/pop().js
@@ -0,0 +1,12 @@
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var obj = fruits.pop();
+//remove last element in the Array
+console.log(obj);
+
+/* op :- Mango */
+
+//after
+console.log(fruits);
+
+/* op :- [ 'Banana', 'Orange', 'Apple' ]*/
+
