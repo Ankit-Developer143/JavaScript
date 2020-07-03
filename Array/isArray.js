6ddec5cbcf63a86fdd8136a27d72dbfb727d5f82
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-03 15:57:42 +0530

    isArray

---
+++ Array/isArray.js
@@ -0,0 +1,12 @@
+
+//isArray Check Array or Not
+
+var str = "Hello how are you";
+var arr = ["How are you"]
+var result = Array.isArray(str)
+var result1 = Array.isArray(arr)
+
+console.log( result);//false
+console.log( result1);//true
+
+
