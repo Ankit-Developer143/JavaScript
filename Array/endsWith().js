8701a5f9058450be44aff98095bde5474a6f500b
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-03 21:12:49 +0530

    endsWith()

--- Array/endsWith().js
+++ Array/endsWith().js
@@ -1,3 +1,16 @@
 const str1 = 'Cats are the best!';
 console.log(str1.endsWith('!'));
-//true
+
+function isPlural(word) {
+    if (word.endsWith('i')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isPlural("hey my name is ankit singh solanki"));//true 
+
+
+const str = (word) => word.endsWith('l') ? true:false;
+
+console.log(str("hello")); //false
