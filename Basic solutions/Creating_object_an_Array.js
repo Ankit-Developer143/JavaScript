fbab0414f93fe64f4a947e8dde4cadb4c86b7b54
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-03 17:55:38 +0530

    convert object an Array'

---
+++ Basic solutions/Creating_object_an_Array.js
@@ -0,0 +1,19 @@
+const Animals = {
+    Dog: 'barking',
+    cat: 'meow'
+
+}
+
+/* Call only keys */
+var ObjectanArray = Object.keys(Animals);
+console.log(ObjectanArray); //op:-[ 'Dog', 'cat' ]
+
+/* Call only Values */
+var ObjectanArray = Object.values(Animals)
+//op:-[ 'barking', 'meow' ]
+
+/* Call Both and object convert to an array */
+var ObjectanArray = Object.entries(Animals)
+
+console.log(ObjectanArray);
+//op:-[ [ 'Dog', 'barking' ], [ 'cat', 'meow' ] ]
