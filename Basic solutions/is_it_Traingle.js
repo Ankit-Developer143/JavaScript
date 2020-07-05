4d7bf93813cb60f5b13c46900a1dec55fc766087
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-05 20:56:32 +0530

    Is it Traingle

---
+++ Basic solutions/is_it_Traingle.js
@@ -0,0 +1,12 @@
+function isItTraingle(a,b,c) {
+
+    if(a+b>c && b+c>a && a+c>b){
+        return true;
+    }else{
+        return false;
+    }
+    
+}
+console.log(isItTraingle(8,18,10));
+
+//Valid  traingle mens sub total 180  
