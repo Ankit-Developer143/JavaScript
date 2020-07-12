1adbb846334c1228d9d02be3e4f3cea1640f5bda
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-12 19:29:33 +0530

    Number.isInteger

--- Basic solutions/Filter Strings from Array.js
+++ Basic solutions/Filter Strings from Array.js
@@ -3,7 +3,7 @@
 //Example filterArray([1, 2, 3, "a", "b", 4]) ➞ [1, 2, 3, 4]
 
 const arr = [1, 2, 3, "a", "b", 4];
-function filterArray(arr){
-	return arr.filter(val => Number.isInteger(val));
+function filterArray(arr) {
+    return arr.filter(val => Number.isInteger(val));
 }
 console.log(filterArray(arr));//op:-[ 1, 2, 3, 4 ]
