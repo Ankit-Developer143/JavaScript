effa934820608a55c30935b51b411a8e4792f49b
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-13 22:43:54 +0530

    Score Checker

--- Basic solutions/Sum of odd Print.js
+++ Basic solutions/Sum of odd Print.js
@@ -1,4 +1,4 @@
- function sumofodd(n){
+/*  function sumofodd(n){
     let a = 0;
     for (let i=1;i<=n;i+=2){  //op: Jump 1,3,5
      a+=i  
@@ -7,3 +7,8 @@
 }
 console.log(sumofodd(5));//OverAll Op:-9 
 
+ */
+
+ 
+function scoreCalculator =(easy, med, hard) =>[...arguments].filter(x =>x<0)?"invalid": easy*5+med*10+hard*20;
+console.log(1,2,3);
