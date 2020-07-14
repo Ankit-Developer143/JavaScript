fcee09d03b39c3e47be82d8690e7e94df00987a2
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-14 15:26:34 +0530

    doubleAfter2Seconds

--- Async Await/doubleAfter2Seconds.js
+++ Async Await/doubleAfter2Seconds.js
@@ -1,8 +1,21 @@
-function doubleAfter2Seconds() {
+ function doubleAfter2Seconds() {
     return new Promise(resolve =>{
         setTimeout(() => resolve(2*2),2000)
 
     });
     
 }
-doubleAfter2Seconds().then(x => console.log(x))  //4
+
+
+//OR
+doubleAfter2Seconds().then(x => console.log(x))  //4 
+
+function doubleAfter2Seconds(x) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(x * 2), 2000)
+
+    });
+
+}
+doubleAfter2Seconds(10) + doubleAfter2Seconds(30)
+    .then(y => console.log(y)) //op;-60
