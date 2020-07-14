1112cd94f5a0b2c6b4c24c9c5669428503af7ac1
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-14 14:58:37 +0530

    Class Method Using Constructor

---
+++ Async Await/Async class methods.js
@@ -0,0 +1,18 @@
+/* class waiter{
+    async wait(){
+        return await Promise.resolve(3+2)
+    }
+}
+new waiter().wait().then(x => console.log(x)); //5 */
+class person {
+    constructor(fname, lname) {
+        this.fname = fname;
+        this.lname = lname;
+    }
+    async greeting() {
+        return await Promise.resolve(` ${this.fname} `);
+
+    }
+
+}
+new person('Ankit', 'Singh').greeting().then(result => console.log(result))//Ankit
