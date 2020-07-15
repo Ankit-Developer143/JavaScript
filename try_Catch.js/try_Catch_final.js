58176274d0896dcd27427155a9deb08897265ce9
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-15 18:32:39 +0530

    try catch final

---
+++ try_Catch.js/try_Catch_final.js
@@ -0,0 +1,27 @@
+/* 
+    try{
+        throw new Error("OOPS!")
+    }
+    catch(e){
+        console.log(e.message);
+    }
+   finally{
+        console.log("finally");
+    }
+ */
+
+function isValidJSON(text) {
+    let a = 10
+    let b = 0;
+    try {
+        const result = b / a;
+        return result;
+
+        return true;
+    } catch (e) {
+        console.log(e.message);;
+    } finally {
+        console.log("finally");
+    }
+}
+console.log(isValidJSON());//op is Finally and 0
