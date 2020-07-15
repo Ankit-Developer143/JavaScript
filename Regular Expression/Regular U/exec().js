ad84f6a8ccc172495aebd0d477a688abc288f092
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-15 20:03:48 +0530

    exec() first Match

--- Regular Expression/Regular U/exec().js
+++ Regular Expression/Regular U/exec().js
@@ -1,9 +1,13 @@
 //exec() return result in an Array
 //if match found then return result if Not found then return null
+//Tests for a match in a string. Returns the first match
 
 let word = /hello/
-/* const result = word.exec('hell') */ //op:-null
+const result = word.exec('hell') //op:-null
+
 const result = word.exec('hello') //op:-[ 'hello', index: 0, input: 'hello', groups: undefined ]
-const result1 = word.exec('hello w')
+
+const result1 = word.exec('hello w')//op:-[ 'hello', index: 0, input: 'hello w', groups: undefined ]
+
 console.log(result);
-console.log(result1);//op:-[ 'hello', index: 0, input: 'hello w', groups: undefined ]
+console.log(result1);
