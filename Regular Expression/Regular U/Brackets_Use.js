a6527b56bd1a7b8ff7dca9fc95f850790e463979
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-16 19:12:33 +0530

    Must Present type Examples

--- Regular Expression/Regular U/Brackets_Use.js
+++ Regular Expression/Regular U/Brackets_Use.js
@@ -1,5 +1,5 @@
 /* USe Brackets [] Character Sets  */
-let re = /gr[ae]y/i;  //either write grey or gray both are optional but (must be Present a or e)
+let re = /gr[ae]y/i;  //either write grey or gray both are optional but in grey (must be Present either a or e)
 re = /[GF]rey/;    //Must be G or F CaseSensitive
 re = /[^GF]rey/;  //Start With any Character except G or F re = /^[GF]rey/ begin with G or F
 re = /[A-Z]/g;   //Start With Any UpperCase  if str = "Xerox" then  op:-[x]
