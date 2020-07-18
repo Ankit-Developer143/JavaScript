7c68e2216628c50e8d5b5881d76564dde2079ccf
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-18 17:54:43 +0530

    Some Advance Concept Id Creator And Iterator Method

---
+++ Iterator/Id_Creator.js
@@ -0,0 +1,13 @@
+function* Iter() {
+  var num = 1;
+  while (true) {
+    yield num++
+  }
+}
+var obj = Iter();
+console.log(obj.next().value);//op:-1
+console.log(obj.next().value);//op:-2
+console.log(obj.next().value);//op:-3
+
+
+
