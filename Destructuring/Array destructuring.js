e864b1473af33480b07262fbf5aecab17118fc0a
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-18 22:22:40 +0530

    Array Destructuring

--- Destructuring/Array destructuring.js
+++ Destructuring/Array destructuring.js
@@ -3,3 +3,9 @@ const [yellow, green, blue] = foo;
 console.log(yellow);//op:-one 
 console.log(green);//op:-two 
 console.log(blue);//op:-three
+
+
+
+const personal = ["Ankit ", 20, "Mumbai"]
+const [Name, id, City] = personal;
+console.log(Name, id, City);//op:-Ankit  20 Mumbai
