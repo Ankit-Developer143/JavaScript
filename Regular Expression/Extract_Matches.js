fec4812d697edf116e24e5ff440ce38bd551b1ed
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-18 20:51:18 +0530

    match words

--- Regular Expression/Extract_Matches.js
+++ Regular Expression/Extract_Matches.js
@@ -1,3 +1,10 @@
-let extractStr = "Extract the word 'coding' from this string.";
+let extractStr = "Extract the word coding from this string.";
 let result = extractStr.match('coding');
 console.log(result);
+/* op:-
+[
+  'coding',
+  index: 17,
+  input: 'Extract the word coding from this string.',
+  groups: undefined
+] */
