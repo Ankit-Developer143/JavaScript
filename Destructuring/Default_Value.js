a025939b0a00c53c335a80f49156dafeaea2ca0d
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-18 21:58:08 +0530

    Default Variables in Destructuring

--- Destructuring/Default_Value.js
+++ Destructuring/Default_Value.js
@@ -1,3 +1,9 @@
-//default Value set in variables
+//default Value set in
 [a =5,b=20] = [1,2];
 console.log(a,b);
+
+
+//Assigning the rest of an array to a variable
+
+[a,...b]=['one', 'two', 'three'];
+console.log(b); //op:-[ 'two', 'three' ]
