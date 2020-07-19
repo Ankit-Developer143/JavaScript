4f6b7ad9d359c7ca2dd2a5c307fe656f26a11e9c
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-19 22:18:20 +0530

    Converting array to set using for loops

--- Set/Array_to_Set_Using_For_loops.js
+++ Set/Array_to_Set_Using_For_loops.js
@@ -1,6 +1,6 @@
 const arr = ['a', 'b', 'b', 'c'];
+const set9 = new Set();
 for (let i = 0; i < arr.length; i++) {
-    const set9 = new Set();
     var result = set9.add(arr[i])
 }
 console.log(result);
