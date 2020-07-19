08699ed1925bf91ca577f52b1c0b2533a578ea46
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-19 21:41:03 +0530

    convert Array to set And Array Use in Set

---
+++ Set/Array_use_in_Sets.js
@@ -0,0 +1,19 @@
+//Array
+const arr = ["Ankit"];
+console.log(Array.isArray(arr));//true
+
+//Array convert in Set  
+const set5 = new Set(arr)
+console.log(set5);//op:-Set { 'Ankit' }
+
+console.log(Array.isArray(set5));//false 
+
+
+
+//Array to set using for of
+for (const item of arr) {
+    const set7 = new Set(item);
+    console.log(set7);
+
+}//op:-Set { 'A', 'n', 'k', 'i', 't' }
+
