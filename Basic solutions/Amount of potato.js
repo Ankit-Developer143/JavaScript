8213fd9e53bd9e346873cdaeae2c8e680c10e9e8
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-31 19:24:57 +0530

    Amount of potato

---
+++ Basic solutions/Amount of potato.js
@@ -0,0 +1,12 @@
+function potatoes(str) {
+   
+   let regex = /potatoes/ig;
+   let result = str.match(regex).length; 
+  console.log(result);
+
+
+    
+	
+}
+let obj = new potatoes("potatoespotatoes");
+
