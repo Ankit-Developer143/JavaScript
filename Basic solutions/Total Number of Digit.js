defac7e28d3cd8036ab99ad7b966831d6ea2b07d
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-08-21 21:31:08 +0530

    int value Length

---
+++ Basic solutions/Total Number of Digit.js
@@ -0,0 +1,14 @@
+function findDigitAmount(num) {
+
+    result = num.toString().split('').length;
+    return result
+
+	
+}
+console.log(findDigitAmount(7154));
+
+/* After Split ['7','1','5','4'] */
+
+//op:-4
+
+
