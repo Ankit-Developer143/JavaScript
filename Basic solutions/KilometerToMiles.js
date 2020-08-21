7cc9edb118863487de57e2f001ee51d85e2acc3d
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-08-21 21:34:07 +0530

    Kilometer To Miles

---
+++ Basic solutions/KilometerToMiles.js
@@ -0,0 +1,12 @@
+//0.621371
+
+function kmtomiles(kilometers) {
+  result = Number.parseFloat((kilometers * 0.621371).toFixed(5));
+  return result;
+}
+console.log(kmtomiles(2));
+
+//only 5 character Show after Decimal its depends on toFixed
+
+//op:-1.24274
+
