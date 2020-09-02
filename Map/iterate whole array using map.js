e5acf45bf93c2f453a22a6f976183031cef17ea9
Ankit-Developer143 <dante7785@gmail.com>
2020-09-02 14:03:29 +0530

    Map

---
+++ Map/iterate whole array using map.js
@@ -0,0 +1,9 @@
+let arr  = [1,2,4,5,5,6,4]
+
+let result = arr.map((val) => val)
+console.log(result);
+
+// op:-[
+//     1, 2, 4, 5,
+//     5, 6, 4
+//   ]
---
+++ Map/negate array using map.js
@@ -0,0 +1,4 @@
+let arr = [1,2,23,4,5,6,7,8,9]
+
+result = arr.map((val) => val *-1)
+console.log(result);
