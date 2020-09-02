ba2a00507878e5d3a950ec2d25f67e30ba813eac
Ankit-Developer143 <dante7785@gmail.com>
2020-09-02 22:29:34 +0530

    Every()

---
+++ Basic solutions/every use in array.js
@@ -0,0 +1,8 @@
+// Fix this incorrect code!
+function checkAllEven(arr) {
+    return arr.every(x =>x % 2 === 0)
+  }
+  console.log(checkAllEven([2,4,6]));
+
+  //Check all value and give boolean Values
+  // op:-true
