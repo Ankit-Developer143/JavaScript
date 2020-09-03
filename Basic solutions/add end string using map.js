e9f376fb832bb2cdf943f5b5dbcfcba52f60e43e
Ankit-Developer143 <dante7785@gmail.com>
2020-09-03 20:34:28 +0530

    Add End of string using map

---
+++ Basic solutions/add end string using map.js
@@ -0,0 +1,8 @@
+function addEnding(arr, ending) {
+    return arr.map((value) => value+ending  )
+   
+	
+}
+console.log(addEnding(["clever", "meek", "hurried", "nice"], "ly"))
+
+//op:-[ 'cleverly', 'meekly', 'hurriedly', 'nicely' ]
